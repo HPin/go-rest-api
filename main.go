41087b5ede061587ad1172fc7ae1c2443c9f73a3
Harald Pinsker <S1910455003@students.fh-hagenberg.at>
2020-06-03 13:47:15 +0200

    hardcoded database credentials and replaced local env variables to make it work with travis

--- main.go
+++ main.go
@@ -2,8 +2,6 @@
 
 package main
 
-import "os"
-
 // entry point for the application
 func main() {
 	// init app
@@ -11,9 +9,9 @@ func main() {
 
 	// init db with credentials stored in environment variables
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		"postgres",
+		"postgres",
+		"postgres")
 
 	// run the app on port 8010
 	a.Run(":8010")
